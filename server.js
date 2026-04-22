d0e270b9e640fa7b2445995351c83977938013e9
StormCode <StormCode@users.noreply.github.com>
2026-04-23 01:57:33 +0800

    修正server.js，指定監聽0.0.0.0

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ if(process.env.NODE_ENV === 'production'){
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
 }
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`server started on port: ${PORT}`)
 });
