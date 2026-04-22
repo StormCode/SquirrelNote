ad83903b8c9d1558b9cd42f36956b3ba6c02f3a2
StormCode <StormCode@users.noreply.github.com>
2026-04-23 05:16:51 +0800

    更新server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
 const app = express();
-const fs = require('fs');
 
 // Setting CORS
 // app.all('*', function(req, res, next) {
@@ -27,26 +26,10 @@ app.use('/api/recyclebin', require('./routes/recyclebin'));
 
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production'){
-    // 根據 outputDirectory 的行為，路徑可能是這兩個其中之一
-    const path1 = path.join(__dirname, 'client', 'build');
-    const path2 = path.join(__dirname, 'build'); // Firebase 有時會提拔到根目錄
-    
-    const buildPath = fs.existsSync(path1) ? path1 : path2;
+    const buildPath = path.join(__dirname, 'client', 'build');
     
     // Set static folder
     app.use(express.static(buildPath));
-    
-
-    // 在 server.js 裡加入這段，直接在網頁上看答案
-    app.get('/debug-path', (req, res) => {
-        res.json({
-            __dirname,
-            buildPath, // 看看路徑對不對
-            exists: fs.existsSync(buildPath),
-            contents: fs.existsSync(buildPath) ? fs.readdirSync(buildPath) : 'Still not found',
-            clientFolder: fs.readdirSync(path.join(__dirname, 'client')) // 看看 client 裡面到底有什麼
-        });
-    });
 
     app.get('*', (req, res) => res.sendFile(path.join(buildPath, 'index.html')));
 }
